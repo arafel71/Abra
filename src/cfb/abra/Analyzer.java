04a5abf8bed2f8b7dcde151e72e4e52d75653d62
Come-from-Beyond <come-from-beyond@mail.ru>
2014-10-03 19:01:50 +0300

    Tokens

---
+++ src/cfb/abra/Analyzer.java
@@ -0,0 +1,57 @@
+package cfb.abra;
+
+/**
+ * (c) 2014 Come-from-Beyond
+ */
+public class Analyzer {
+
+    public static final String[] TOKENS = {
+
+            "!",
+            "!=",
+            "%",
+            "&",
+            "&&",
+            "(",
+            ")",
+            "*",
+            "*/",
+            "+",
+            ",",
+            "-",
+            ".",
+            "/",
+            "/*",
+            "//",
+            ":",
+            ";",
+            "<",
+            "<=",
+            "=",
+            "==",
+            ">",
+            ">=",
+            "[",
+            "]",
+            "break",
+            "case",
+            "continue",
+            "default",
+            "do",
+            "else",
+            "for",
+            "if",
+            "nonet",
+            "return",
+            "singlet",
+            "struct",
+            "switch",
+            "triplet",
+            "void",
+            "while",
+            "{",
+            "|",
+            "||",
+            "}"
+    };
+}
